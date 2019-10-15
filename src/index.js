fd30aec5bdd493bc65a8299b49cae6a8b8d89ffe
Babiosik <babiosik.satan@gmail.com>
2019-10-15 20:21:54 +0300

    make on webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
+import { Scene } from 'three';
+
+const scene = new Scene();
+/*
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+*/
