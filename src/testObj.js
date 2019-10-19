c976fbed45a343c4faa5ebf84aa13a0ecfdc539c
Olezha Olezha <matyushchenkk@outlook.com>
2019-10-19 18:01:50 +0300

    hello, world!

--- src/testObj.js
+++ src/testObj.js
@@ -12,27 +12,26 @@ class TestObj {
         let y = pos.y;
         let z = pos.z - 100;
 
-        let i = 0;
+        let i = 1;
+        //console.log(trashs.length); 33
+
         let name = trashs[i].filename;
         let scale = (trashs[i].scale + Math.random() - 0.5) * 1.5;
         this.speedRotate = 0.2;
 
-        this.phantom = new Mesh( new SphereBufferGeometry( 1, 1, 1 ), new MeshBasicMaterial( { color: 0x000000 } ));
+        this.phantom = new Mesh( new SphereBufferGeometry( 10, 3, 3), new MeshBasicMaterial( { color: 0x000000 } ));
 
         const self = this;
+        console.log("Tut")
         loader.load('/models/trash/' + name, function ( geometry ) {
-
-            let meshMaterial = new MeshBasicMaterial( { color: 0xffffff } );
-            if ( geometry.hasColors ) {
-                meshMaterial = new MeshPhongMaterial( { opacity: geometry.alpha, vertexColors: VertexColors } );
-            } else if (trashs[i].texture != '') {
-                meshMaterial = new MeshPhongMaterial( {
-                    map: textureLoader.load( "/models/trash/" + trashs[i].texture )
-                } );
-            }
-            let mesh = new Mesh( geometry, meshMaterial );
-            mesh.scale.set( scale, scale, scale );
-
+            let meshMaterial = new MeshBasicMaterial( {
+                map: textureLoader.load( "/textures/planets/earth_atmos_4096.jpg" ),
+                specularMap: textureLoader.load( "/textures/planets/earth_specular_2048.jpg" ),
+            } );
+            console.log("Ge")
+            let mesh = new Mesh( new SphereBufferGeometry( 100, 777, 156 ), new MeshBasicMaterial( { color: 0xffffff }) );
+            //mesh.scale.set( scale, scale, scale );
+         
             self.mesh = mesh;
             self.phantom.add(mesh);
             self.phantom.position.x = x;
