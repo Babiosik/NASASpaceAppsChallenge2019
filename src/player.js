84857a8a3f59fb1944d7c22b925142608acf79c9
Babiosik <valor666.geldar@gmail.com>
2019-10-19 17:51:56 +0300

    claw anim

--- src/player.js
+++ src/player.js
@@ -7,6 +7,9 @@ class Player {
         this.phantom = new Mesh( new BoxBufferGeometry( 1, 1, 1 ), new MeshBasicMaterial( { color: 0x005500, } ) );
         this.phantom.add(this.camera);
         this.warningTrash = [];
+
+        this.clawAnimStage = 0;
+        this.isClawClosed = false;
         //this.camera.translateZ(-1);
 
         this.renderer = new WebGLRenderer( { antialias: true } );
@@ -22,14 +25,22 @@ class Player {
 
         let leftHand = new Mesh( new BoxBufferGeometry( 0.2, 0.2, 8 ), new MeshBasicMaterial( { color: 0xaaaaaa } ) );
         let rightHand = new Mesh( new BoxBufferGeometry( 0.2, 0.2, 8 ), new MeshBasicMaterial( { color: 0xaaaaaa } ) );
+        let leftHandClaw = new Mesh( new BoxBufferGeometry( 0.3, 0.3, 2 ), new MeshBasicMaterial( { color: 0x888888 } ) );
+        let rightHandClaw = new Mesh( new BoxBufferGeometry( 0.3, 0.3, 2 ), new MeshBasicMaterial( { color: 0x888888 } ) );
 
-        this.pickup = new Mesh( new BoxBufferGeometry( 1, 1, 1 ), new MeshBasicMaterial( { color: 0x005500, } ) );
-        
-        this.phantom.add(leftHand, rightHand, this.pickup);
+        this.pickup = new Mesh( new BoxBufferGeometry( 1, 1, 1 ), new MeshBasicMaterial( { color: 0x005500, transparent: true, opacity: 0.1 } ) );
+        this.leftHandClaw = leftHandClaw;
+        this.rightHandClaw = rightHandClaw;
+
+        this.phantom.add(leftHand, rightHand, this.leftHandClaw, this.rightHandClaw, this.pickup);
         
         leftHand.position.x = -3; rightHand.position.x =  3; this.pickup.position.x =  0;
         leftHand.position.y =     rightHand.position.y = -1; this.pickup.position.y = -1;
         leftHand.position.z =     rightHand.position.z = -4; this.pickup.position.z = -7;
+
+        leftHandClaw.position.x = -3; rightHandClaw.position.x =  3;
+        leftHandClaw.position.y =     rightHandClaw.position.y = -1;
+        leftHandClaw.position.z =     rightHandClaw.position.z = -8;
 
     }
     addWarningObject(obj) {
@@ -41,7 +52,50 @@ class Player {
     hasWarningObject(obj) {
         return this.warningTrash.indexOf(obj) != -1;
     }
+    clawClose() {
+        if (this.isClawClosed)
+            return;
+        this.isClawClosed = true;
+        this.clawAnimStage = -1;
+    }
 
+    update(delta) {
+        this.controls.update( delta );
+
+        if (this.clawAnimStage != 0) {
+            switch(this.clawAnimStage) {
+                case -1:
+                    this.leftHandClaw.rotateY(this.clawAnimStage * 0.8 * delta);
+                    this.rightHandClaw.rotateY(this.clawAnimStage * -0.8 * delta);
+                    if (this.leftHandClaw.rotation.y < -1.5) {
+                        this.clawAnimStage = -2;
+                    }
+                    break;
+                case -2:
+                    this.leftHandClaw.translateZ(this.clawAnimStage * 0.5 * delta);
+                    this.rightHandClaw.translateZ(this.clawAnimStage * 0.5 * delta);
+                    if (this.leftHandClaw.position.z < -8.05) {
+                        this.clawAnimStage = 2;
+                    }
+                    break;
+                case 1:
+                        this.leftHandClaw.rotateY(this.clawAnimStage * 0.8 * delta);
+                        this.rightHandClaw.rotateY(this.clawAnimStage * -0.8 * delta);
+                        if (this.leftHandClaw.rotation.y > 0) {
+                            this.clawAnimStage = 0;
+                            this.isClawClosed = false;
+                        }
+                        break;
+                case 2:
+                    this.leftHandClaw.translateZ(this.clawAnimStage * 0.5 * delta);
+                    this.rightHandClaw.translateZ(this.clawAnimStage * 0.5 * delta);
+                    if (this.leftHandClaw.position.z > -8) {
+                        this.clawAnimStage = 1;
+                    }
+                    break;
+            }
+        }
+    }
 }
 
 export default Player;
